9dedf261caeb74c512dcd0127e6e89e2992ab1f2
mtopart <m.topart@agro-transfert-rt.org>
2022-05-20 17:10:23 +0200

    MAJ

--- R/app_server.R
+++ R/app_server.R
@@ -7,4 +7,8 @@
 app_server <- function( input, output, session ) {
   # Your application server logic 
   
+  
+  mod_presentation_server("presentation_ui_1")
+  mod_situation_generale_server("situation_generale_ui_1")
+  
 }
